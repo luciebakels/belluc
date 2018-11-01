ce2a15fd1989dc28ec489adb4271c19e68331b8a
Lucie Bakels <lucie_bakels@hotmail.com>
2018-11-01 14:35:54 +0800

    print functions

--- source/belluc/__init__.py
+++ source/belluc/__init__.py
@@ -3,32 +3,37 @@ from astropy.io import fits
 
 
 class FitsFile:
-	def __init__(self, fname):
-		self.fname = fname
-		self.file = None
+    def __init__(self, fname):
+        self.fname = fname
+        self.file = None
+        self.ra = None
+        self.dec = None
 
-	def openFile(self):
-		if self.file is None:
-			self.file = fits.open(self.fname)
-		else:
-			return 0
+    def openFile(self):
+        if self.file is None:
+            self.file = fits.open(self.fname)
+        else:
+            return 0
 
-	def closeFile(self):
-		if self.file is None:
-			return 0
-		else:
-			self.file.close()
-			self.file = None
+    def closeFile(self):
+        if self.file is None:
+            return 0
+        else:
+            self.file.close()
+            self.file = None
 
-	def printNumberHDUs(self):
-		self.openFile()
-		
-		num_hdus = len(self.file)
-		print("This file has {} HDUs.".format(num_hdus))		
+    def printNumberHDUs(self):
+        self.openFile()
+        
+        num_hdus = len(self.file)
+        print("This file has {} HDUs.".format(num_hdus))        
 
-		self.closeFile()
+        self.closeFile()
     
     def checkRA(self):
+        if self.ra is not None:
+            return 0
+
         self.openFile()
         
         self.ra = self.file[0].header["RA"]        
@@ -36,8 +41,19 @@ class FitsFile:
         self.closeFile()
 
     def checkDec(self):
+        if self.dec is not None:
+            return 0
+
         self.openFile()
         
         self.dec = self.file[0].header["DEC"]        
         
         self.closeFile() 
+
+    def printRA(self):
+        self.checkRA()
+        print('RA: ', self.ra)
+
+    def printDEC(self):
+        self.checkDec()
+        print('DEC: ', self.dec)
